4369c179eb7bafb82be589dc045c746600dd5ef9
f.taleb <farzanehtaleb11@gmail.com>
2017-10-04 11:50:53 +0330

    ADD WORK AND CATEGORY

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import {RouterModule} from "@angular/router";
 import {TaskComponent} from "./tasks/task/task.component";
+import {CategoryComponent} from "./categories/category/category.component";
 /**
  * Created by f.taleb on 9/3/2017.
  */
@@ -7,7 +8,8 @@ const APP_ROUTES_PROVIDER =[
 
 
   // { path:'' , redirectTo:'/tasks' ,pathMatch : 'full'} ,
-  { path:'tasks' , component: TaskComponent }
+  { path:'tasks' , component: TaskComponent },
+  { path:'categories' , component: CategoryComponent },
 
 ]
 export const routing = RouterModule.forRoot(APP_ROUTES_PROVIDER) ;
